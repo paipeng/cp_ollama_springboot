d76acf9ce138d8859c958103999177584c4890fe
Pai Peng - Ubuntu-Cuda <sipaipv6@gmail.com>
2025-12-16 14:23:12 +0800

    works with model for translating via chat-controller

--- src/main/java/com/paipeng/ollama/config/OllamaConfig.java
+++ src/main/java/com/paipeng/ollama/config/OllamaConfig.java
@@ -1,10 +1,8 @@
 package com.paipeng.ollama.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 class OllamaConfig {
-
 
 }
---
+++ src/main/java/com/paipeng/ollama/controller/TranslateController.java
@@ -0,0 +1,23 @@
+package com.paipeng.ollama.controller;
+
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.ollama.api.OllamaApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
+
+@RestController
+public class TranslateController {
+    @Autowired
+    private OllamaChatModel chatModel;
+
+
+    
+
+}
