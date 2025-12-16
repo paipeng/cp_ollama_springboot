4cf51fd31581b5a48aa2165b3d0b81ad5e678c62
Pai Peng - Ubuntu-Cuda <sipaipv6@gmail.com>
2025-12-16 13:50:37 +0800

    spring ai ollama testing

---
+++ src/main/java/com/paipeng/ollama/config/H2DataSourceConfig.java
@@ -0,0 +1,24 @@
+package com.paipeng.ollama.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class H2DataSourceConfig {
+/*
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:customDb;DB_CLOSE_DELAY=-1"); // DB_CLOSE_DELAY=-1 keeps the DB alive
+        dataSource.setUsername("sa");
+        dataSource.setPassword("password");
+        return dataSource;
+    }
+
+ */
+}
---
+++ src/main/java/com/paipeng/ollama/config/OllamaConfig.java
@@ -0,0 +1,10 @@
+package com.paipeng.ollama.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class OllamaConfig {
+
+
+}
